95e3c93837d5d046cea522d7b7b0d60547bcbee1
ivan.lazarte@gmail.com <ilazarte@users.noreply.github.com>
2018-11-19 04:10:35 -0500

    section 2

---
+++ ppmtool/src/main/java/com/neondigit/ppmtool/services/MapValidationErrorService.java
@@ -0,0 +1,27 @@
+package com.neondigit.ppmtool.services;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class MapValidationErrorService {
+
+    public ResponseEntity<?> mapValidationService(BindingResult result) {
+
+        if (result.hasErrors()) {
+            Map<String, String> errorMap = new HashMap<>();
+            result.getFieldErrors().forEach( e -> {
+                errorMap.put(e.getField(), e.getDefaultMessage());
+            });
+
+            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+        }
+
+        return null;
+    }
+}
---
+++ ppmtool/src/main/java/com/neondigit/ppmtool/services/ProjectService.java
@@ -0,0 +1,53 @@
+package com.neondigit.ppmtool.services;
+
+import com.neondigit.ppmtool.domain.Project;
+import com.neondigit.ppmtool.exceptions.ProjectIdException;
+import com.neondigit.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project) {
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID " + project.getProjectIdentifier().toUpperCase() + " already exists.");
+        }
+    }
+
+    public Project findProjectByIdentifier(String projectId) {
+
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if (project == null) {
+            throw new ProjectIdException("Project Id '" +
+                    projectId +
+                    "' does not exist");
+        }
+
+        return project;
+    }
+
+    public Iterable<Project> findAllProjects() {
+        return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if (project == null) {
+            throw new ProjectIdException("Cannot load project with id '" +
+                    projectId +
+                    "' This project does not exist");
+        }
+
+        projectRepository.delete(project);
+    }
+}
